418f99934c7547ab1390607f4c53ea609cbbaa42
Z3r0byte <ditisvoorspambas@gmail.com>
2014-12-18 19:29:37 +0100

    check or permission
    This is a very simple script to check if the user is allowed to come here.

--- protected_page.php
+++ protected_page.php
@@ -19,27 +19,26 @@ include_once 'includes/db_connect.php';
 include_once 'includes/functions.php';
 
 sec_session_start();
+$hoi = hoi
 ?>
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Secure Login: Protected Page</title>
+        <title>vdboom</title>
         <link rel="stylesheet" href="styles/main.css" />
     </head>
     <body>
-        <?php if (login_check($mysqli) == true) : ?>
+        <?php if (login_check($mysqli) == true && ($_SESSION['username']) == "username" || "other username") : ?>
         <p>Welcome <?php echo htmlentities($_SESSION['username']); ?>!</p>
+            <p>Go back to the <a href="login.php">login page.</a></p>
+        <?php elseif(login_check($mysqli) == true) : ?>
             <p>
-                This is an example protected page.  To access this page, users
-                must be logged in.  At some stage, we'll also check the role of
-                the user, so pages will be able to determine the type of user
-                authorised to access the page.
+                <span class="error">Access denied! You don't have permissions to come here!</span>.
             </p>
-            <p>Return to <a href="index.php">login page</a></p>
         <?php else : ?>
             <p>
-                <span class="error">You are not authorized to access this page.</span> Please <a href="index.php">login</a>.
+                <span class="error">Access denied.</span><a href="login.php"> Please, Log in</a>.
             </p>
         <?php endif; ?>
     </body>
